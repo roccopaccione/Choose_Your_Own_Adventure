2e6920c9367c151b1ed24e40744cb4250c7e0de4
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-10-18 20:56:18 -0500

    feat: added gameplay settings and fighter selection print

--- src/EnterBlueprint.java
+++ src/EnterBlueprint.java
@@ -9,11 +9,23 @@ public class EnterBlueprint {
         String userName = scan.next();
 
         System.out.printf("Hello %s, Glad to see you would like to tempt your fate...", userName);
+    }
 
+    public void fighterSelector(){
         System.out.println("Please %s, choose your type of hero!\n" +
-                "");
-        int typeHero = scan.nextInt();
+                "1 - Shining Knight\n" +
+                "2 - Shonen Warrior\n" +
+                "3 - Kamikaze\n" +
+                "4 - Battle Warrior\n" +
+                "5 - Sprinting Freelance");
 
-
+    }
+    public void knightGameplay(){
+        System.out.println("Where would you like to start?\n" +
+                "Mountain Top - A road of fortunes or misfortunes embedded in a towering frozen tundra." +
+                "City Streets - A barren city abandoned of all virtuous beings with surprises around every corner." +
+                "Forest of Futility - A dark magical forest containing menacing beings who's only objective is your demise." +
+                "Highrise - A towering inferno of challenges that can make the sanest person on the planet go mad in minutes" +
+                "Pueblo - Native village with outlaws and virtue seekers looking for your head.");
     }
 }
